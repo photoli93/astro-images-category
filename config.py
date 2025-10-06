b2f0339264f0e4f96af979e70c3032d516bf56c7
Olivier Liu <liuolivier93@gmail.com>
2025-10-06 23:01:07 +0200

    create config.py file with all constants, device config, seed and paths in it

---
+++ config.py
@@ -0,0 +1,48 @@
+# config.py
+from pathlib import Path
+import torch
+import numpy as np
+import random
+
+# =====================================================================
+# Device configuration
+# =====================================================================
+if torch.backends.mps.is_available():
+    DEVICE = torch.device("mps")
+elif torch.cuda.is_available():
+    DEVICE = torch.device("cuda")
+else:
+    DEVICE = torch.device("cpu")
+
+# =====================================================================
+# Paths
+# =====================================================================
+ORIGINAL_DATA_PATH = Path("../space-images-category")
+OUTPUT_PATH = Path("../space_images_split")
+
+# =====================================================================
+# Split ratios
+# =====================================================================
+TRAIN_RATIO = 0.70
+VAL_RATIO = 0.15
+TEST_RATIO = 0.15
+
+# =====================================================================
+# Hyperparameters
+# =====================================================================
+IMG_SIZE = 224
+BATCH_SIZE = 32
+NUM_WORKERS = 4
+EPOCHS_STAGE1 = 25
+EPOCHS_STAGE2 = 15
+LEARNING_RATE_STAGE1 = 1e-3
+LEARNING_RATE_STAGE2 = 1e-5
+
+# =====================================================================
+# Seed
+# =====================================================================
+SEED = 42
+torch.manual_seed(SEED)
+np.random.seed(SEED)
+if DEVICE.type == "mps":
+    torch.mps.manual_seed(SEED)
